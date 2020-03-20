1018d50a0f5cfb2e3f5e92fb35838b49888294a0
Juan Hernandez <juanhernandez@Juans-iMac.local>
2020-03-20 12:59:18 -0600

    added java field to extract config params

--- selenium-demo/src/main/java/st/pages/BasePage.java
+++ selenium-demo/src/main/java/st/pages/BasePage.java
@@ -3,9 +3,12 @@ package st.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import st.util.BaseConfig;
+
 public class BasePage{
-    
+
     private WebDriver driver;
+    private static final BaseConfig CONFIG = BaseConfig.getConfig();
 
     public BasePage(WebDriver driver){
         this.driver = driver;
