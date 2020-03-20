24b845c96aa94a9e95fc4cc538cd2dd659318562
Juan Hernandez <juanhernandez@Juans-iMac.local>
2020-03-20 12:14:56 -0600

    added before hook.

--- selenium-demo/src/test/java/st/Steps.java
+++ selenium-demo/src/test/java/st/Steps.java
@@ -1,11 +1,17 @@
 package st;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Steps {
+
+    @Before({"@with_browser"})
+    public void setUp(){
+        System.out.println("hook seen!");
+    }
     
     @Given("^precondition$")
     public void precondition() throws Throwable {
