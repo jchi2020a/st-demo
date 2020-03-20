facda78ed281dee0f41aada7921392bebb241170
Juan Hernandez <juanhernandez@Juans-iMac.local>
2020-03-20 13:39:38 -0600

    added method impl to search query.

--- selenium-demo/src/main/java/st/pages/ComponentReferencePage.java
+++ selenium-demo/src/main/java/st/pages/ComponentReferencePage.java
@@ -1,13 +1,24 @@
 package st.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ComponentReferencePage{
-    private WebDriver driver; 
+    private WebDriver driver;
+    
+    @FindBy(css = "input[type='search']")
+    WebElement serchField;
     
     public ComponentReferencePage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public ComponentReferencePage searchComponent(String query){
+        serchField.clear();
+        serchField.sendKeys(query);
+        return this;
     }
 }
