829e34f46320e71ef0db7b46e4d5647d11d3bcd7
Juan Hernandez <juanhernandez@Juans-iMac.local>
2020-03-20 12:58:24 -0600

    added class constructor

--- selenium-demo/src/main/java/st/pages/BasePage.java
+++ selenium-demo/src/main/java/st/pages/BasePage.java
@@ -1,7 +1,14 @@
 package st.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class BasePage{
     
+    private WebDriver driver;
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
