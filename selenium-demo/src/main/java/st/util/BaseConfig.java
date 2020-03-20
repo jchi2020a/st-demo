3c56e49c0c44dc0595c490c4f64b56e98a20aca1
Juan Hernandez <juanhernandez@Juans-iMac.local>
2020-03-20 12:33:44 -0600

    method to get config singleton

--- selenium-demo/src/main/java/st/util/BaseConfig.java
+++ selenium-demo/src/main/java/st/util/BaseConfig.java
@@ -1,10 +1,15 @@
 package st.util;
 
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.typesafe.config.ConfigFactory;
 
 public class BaseConfig{
     private static final ObjectMapper MAPPER = buildMapper();
+    private static final Map<String, Object> unwrappedConfig = ConfigFactory.load().root().unwrapped();
+    private static final BaseConfig CONFIG = buildConfSingleton(unwrappedConfig);
 
     private String baseUrl;
 
@@ -15,9 +20,20 @@ public class BaseConfig{
         return baseUrl;
     }
 
+    /**
+     * @return the config
+     */
+    public static BaseConfig getConfig() {
+        return CONFIG;
+    }
+
     private static ObjectMapper buildMapper(){
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
+    }
+
+    private static BaseConfig buildConfSingleton(Map<String, Object> unwrappedConfig){
+        return MAPPER.convertValue(unwrappedConfig, BaseConfig.class);
     }
 }
