e2e76611fd40409066c41078529985c6474967af
Juan Hernandez <juanhernandez@Juans-iMac.local>
2020-03-20 12:10:15 -0600

    added step defs class (method signatures included)

---
+++ selenium-demo/src/test/java/st/Steps.java
@@ -0,0 +1,27 @@
+package st;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Steps {
+    
+    @Given("^precondition$")
+    public void precondition() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^action$")
+    public void action() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^testable outcome$")
+    public void testable_outcome() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
